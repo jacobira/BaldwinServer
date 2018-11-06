1394d55fe83a075e8c0a6350381af69a20e95fc1
Jacob Baldwin <jacobbaldwin@Jacobs-MacBook-Pro.local>
2018-11-06 14:42:07 -0700

    NOW user login working, disregard last commit message. File dump/edit function incorporated on client-side code.

--- BaldwinServer1.0Client.js
+++ BaldwinServer1.0Client.js
@@ -1,23 +1,29 @@
 const io = require('socket.io-client');
 const client = io.connect('http://localhost:3333');
 
-var auth = false;
-var usernameEntry = "development";
+var usernameEntry = "jacob";
 var passwordEntry = "password";
 
-client.emit("login", (usernameEntry, passwordEntry));
-client.on("keycard", (username) => {
+client.emit("login", [`${usernameEntry}`, `${passwordEntry}`]);
+
+client.on("keycard", (data) => {
     // allows access to full front-end and backend functions.
     auth = true;
-});
-
-if (auth = true) {
 
     var username = usernameEntry;
 
     function filePull() {
         client.emit("file-pull", request);
     };
+    function fileDump(filename, contents) {
+        client.emit("file-dump", [filename, contents]);
+    }
     
     client.emit("message-up", `Hello, server! Username ${username} now active.`);
-};
+    client.on("message-down", (data) => {
+        console.log(data);
+    });
+
+    fileDump("test.txt", "Testing, testing. From client " + `username ${username}`);
+    
+});
--- BaldwinServer1.0Server.js
+++ BaldwinServer1.0Server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const fs = require('fs');
 
 var port = 3333;
-var users = [{name: 'jacob', password: 'password', active: false},
-	{name: 'kira', password: 'password', active: false},
-	{name: 'development', password: 'password', active: false}];
+var users = [{username: 'jacob', password: 'password', active: false, name: 'Jacob'},
+	{username: 'kira', password: 'password', active: false, name:'Kira Jo'},
+	{username: 'development', password: 'password', active: false, name:'Developer'}];
 
 // ideally, the above user information would be stored in an external database
 // with which this backend would be communicating - Especially in cases where
@@ -17,27 +18,28 @@ var users = [{name: 'jacob', password: 'password', active: false},
 io.on('connection', (socket) => {
 	var socketUser = '';
 	
-	// need to build front end code so that it passes the below listed parameters.
-	socket.on('login', (name, password)=> {
+	socket.on('login', (data) => {
 
+		var username = data[0];
+		var password = data[1];
 		var auth = false;
-		var authAcc;
+		var authAcc = -1;
 
 		for (let i = 0; i < users.length; i++) {
-			if ((name = users[i].name) && (password = users[i].password)) {
+			if (username == users[i].username && password == users[i].password) {
 				auth = true;
 				authAcc = i;
 			};
 		};
-		// Log in using development account
 		
-		if (auth = true) {
-			users[authAcc].active = true;
-			socketUser = `${users[authAcc].name}`;
+		
+		if (auth == true) {
 			socket.emit('keycard', socketUser); 
-			// Front end needs to accept this above event as approval
-			// from the backend and allow access to backend functions.
-			socket.emit('message-down', `Welcome to BaldwinServer 1.0, ${users[authAcc].name}!`);
+			users[authAcc].active = true;
+			// console.log("if statement is working");
+			
+			socketUser = `${users[authAcc].name}`;
+			socket.emit('message-down', `Welcome to (c)BaldwinServer 1.0, ${socketUser}!`);
 			
 			// Events and listeners below
 			socket.on('message-up', (data) => {
@@ -48,18 +50,28 @@ io.on('connection', (socket) => {
 				console.log(`File pull backend has not yet been built...Sorry!`);
 			});
 			socket.on('file-dump', (data) => {
-				console.log(`File dump initiated`);
-				console.log(`File dump backend has not yet been built...Sorry!`);
+				// syntax for writing files:
+				// fs.writeFile(<filename.txt>, <filecontents>, <function(err)>)
+				fs.writeFile(`${data[0]}`, `${data[1]}`, (err) => {
+					if (err) {
+						console.log('There was an error dumping the file');
+					}
+					else {
+						console.log('File dump initiated...');
+						socket.emit('message-down', 'File dump initiated on server...');
+					}
+				});
 			});
 			socket.on('new-sched-item', (data) => {
 				console.log(`New schedule item initiated`);
 				console.log(`New schedule item backend has not yet been built...Sorry!`);
 			});
-			}
-			else {
+		}
+
+		else {
 			socket.emit('message', `Incorrect login information.`);
 			console.log(`Failed login attempt.`);
-			};
+		};
 		
 	});
 });
@@ -69,5 +81,10 @@ io.on('error', (err) => {
 	console.log(err);
 });
 
+io.on('end', () => {
+	console.log('User disconnected');
+});
+
+// Port listener and confirmation.
 server.listen(port);
 console.log(`Server now running on localhost:${port}`);
